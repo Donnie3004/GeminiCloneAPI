dcc910e28f939fcc438183872f06773df26277e0
Donnie3004 <arbazrehman3004@gmail.com>
2025-07-20 19:46:30 +0530

    Final-2A-Commit

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,12 +1,26 @@
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// // const redis = new Redis({
+// //   host: process.env.REDIS_HOST, 
+// //   port: Number(process.env.REDIS_PORT),      
+// // });
+
+// const redis = new Redis(process.env.REDIS_URL)
+
+// export default redis;
+
+import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const redis = new Redis({
-//   host: process.env.REDIS_HOST, 
-//   port: Number(process.env.REDIS_PORT),      
-// });
+const redis = createClient({
+  url: process.env.REDIS_URL
+});
 
-const redis = new Redis(process.env.REDIS_URL)
+redis.on('error', (err) => console.error('Redis Client Error:', err));
+
+await redis.connect(); // Use inside async context only
 
 export default redis;
